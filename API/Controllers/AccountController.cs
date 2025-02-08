9fda7bca33322204d48bece21ef037a0e8262050
bartstc <bartst010395@gmail.com>
2025-02-08 12:30:53 +0100

    Handle login with JWT token

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+using API.Dtos;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly TokenService _tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _userManager = userManager;
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (result)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _tokenService.CreateToken(user),
+                    Image = null
+                };
+            }
+
+            return Unauthorized();
+        }
+    }
+}
